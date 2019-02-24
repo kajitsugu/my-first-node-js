9bd979e7c1c2786354b9364a154692b94376f49b
kajitsugu <kajitsugu@gmail.com>
2019-02-24 05:53:11 +0000

    first commit

---
+++ workspace/my-first-shell/mk-template-dir.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+read -p "テンプレートディレクトリのディレクトリ名を入力して下さい" dir
+mkdir ~/workspace/my-first-shell/$dir
+mkdir ~/workspace/my-first-shell/$dir/1
+mkdir ~/workspace/my-first-shell/$dir/2
+mkdir ~/workspace/my-first-shell/$dir/3
